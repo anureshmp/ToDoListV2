5d8a0940104d52e6aa541dc2442cde6445ad701b
anureshmp <anureshmp@gmail.com>
2026-08-05 10:44:53 +1000

    Added delete task functionality

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, EventEmitter, Input, input, Output } from '@angular/core';
 import { Task } from './task.model';
 
 
@@ -12,5 +12,13 @@ import { Task } from './task.model';
 })
 export class TaskComponent {
   @Input({required: true}) task!: Task
+  @Output() complete = new EventEmitter<string>();
+
+  onCompleteTask()
+  {
+    this.complete.emit(this.task.id);
+  }
+
+
 
 }
