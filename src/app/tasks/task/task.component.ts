3b8ad4a76f7a949d80365a41aff2f96bd5e6a0c8
anureshmp <anureshmp@gmail.com>
2026-08-05 12:55:31 +1000

    Date formatting using pipe

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,12 +1,13 @@
-import { Component, EventEmitter, Input, input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, input, Output, Pipe } from '@angular/core';
 import { Task } from './task.model';
+import { DatePipe } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-task',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css'
 })
